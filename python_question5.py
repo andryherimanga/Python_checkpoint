1a20144334337c3bf7eb3c4b757a25f238068076
andryherimanga <99554913+andryherimanga@users.noreply.github.com>
2022-02-16 11:26:55 +0000

    Add files via upload

---
+++ python_question5.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+original=np.array([[0,1],[2,3],[4,5]])
+array_to_list=original.tolist()
+print("Original array elements: ",original)
+print("Array converted to list: ",array_to_list)
